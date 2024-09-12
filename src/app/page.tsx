8ce06456c1d3dabe1c21382b721eb0c9680de000
mguleryuz <mguleryuz3@gmail.com>
2024-09-12 14:17:39 -0500

    demonstrate slug and non-slug behaivour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default async function Home() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <Image
-        src={`/api/image/${"vercel.png"}`}
-        width="800"
-        height="184"
-        alt=""
-      />
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="slug">With Slug IMG Route</TabsTrigger>
+          <TabsTrigger value="non-slug">Without Slug IMG Route</TabsTrigger>
+        </TabsList>
+        <TabsContent value="slug">
+          <Image
+            src={`/api/image/${"vercel.png"}`}
+            width="800"
+            height="184"
+            alt=""
+          />
+        </TabsContent>
+        <TabsContent value="non-slug">
+          <Image src={`/api/image`} width="800" height="184" alt="" />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
